9f1f5c5ff5c983c2fde839df6ebdf219ce635a4f
Beau Holton <zod000@yahoo.com>
2017-07-14 16:53:16 -0400

    Add replacement for newid() default values

--- sqlserver2pgsql.pl
+++ sqlserver2pgsql.pl
@@ -495,6 +495,7 @@ sub convert_transactsql_code
 	$code =~ s/getdate\s*\(\)/CURRENT_TIMESTAMP/gi;
 	$code =~ s/user_name\s*\(\)/CURRENT_USER/gi;
 	$code =~ s/datepart\s*\(\s*(.*?)\s*\,\s*(.*?)\s*\)/date_part('$1', $2)/gi;
+  $code =~ s/newid\s*\(\)/uuid_generate_v4()/gi;
 	return $code;
 }
 
